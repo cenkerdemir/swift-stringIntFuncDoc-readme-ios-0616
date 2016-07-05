3b7d45762842cec0310a0528ced6052e2f1fb619
github username <github email address>
2016-07-05 16:33:55 -0400

    completed

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -4,3 +4,7 @@ name.uppercaseString
 let uppercaseName = name.uppercaseString
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+let newName = uppercaseName.lowercaseString
+
+print("now \(newName)'s name is all lowercase")
